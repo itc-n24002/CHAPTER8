abb92e195431508eb514a7d1e430cd5f15d45f6d
n24002 <n24002@std.it-college.ac.jp>
2024-08-22 06:08:03 +0000

    aa

---
+++ Q8_10_1.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicCconfig(filenamae='practice1.log', level=logging.WARNING
+logging.warning('1')
+logging.error('2')
+loggong.debug('3')
+logging.info('4')
+logging.shutdown()
